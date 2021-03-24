ba9202b215e8ede76196e81c1bcd16fe82ace6ab
Shikhar15606 <shikharshukla15606@gmail.com>
2021-03-25 02:04:52 +0530

    Added Fonts

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 import productsReducer from './store/reducers/products';
 
@@ -14,18 +14,16 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <Provider store={store}>
       <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
