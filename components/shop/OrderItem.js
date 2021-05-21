891cdbbcff23fc57512ec27e4f659ca287c66d24
Shikhar15606 <shikharshukla15606@gmail.com>
2021-05-22 02:54:35 +0530

    Show Order Details

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import { CartItem } from './CartItem';
+import CartItem from './CartItem';
 import Colors from '../../constants/Colors';
 
 const OrderItem = props => {
+  const [showDetails, setShowDetails] = useState(false);
   return (
     <View style={styles.orderItem}>
       <View style={styles.summary}>
         <Text style={styles.totalAmount}>$ {props.amount.toFixed(2)}</Text>
         <Text style={styles.date}>{props.date}</Text>
       </View>
-      <Button color={Colors.primary} title='Show Details' />
+      <Button
+        onPress={() => {
+          setShowDetails(prevState => !prevState);
+        }}
+        color={Colors.primary}
+        title={showDetails ? 'Hide Details' : 'Show Details'}
+      />
+      {showDetails && (
+        <View style={styles.detailItem}>
+          {props.items.map(cartItem => (
+            <CartItem
+              key={cartItem.productId}
+              quantity={cartItem.quantity}
+              amount={cartItem.sum}
+              title={cartItem.productTitle}
+            />
+          ))}
+        </View>
+      )}
     </View>
   );
 };
@@ -43,6 +62,9 @@ const styles = StyleSheet.create({
     fontFamily: 'open-sans',
     fontSize: 16,
     color: '#888',
+  },
+  detailItem: {
+    width: '100%',
   },
 });
 export default OrderItem;
