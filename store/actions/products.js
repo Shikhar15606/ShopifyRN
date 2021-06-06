312194058a330c8f0623eed0a7ccf6d9b728a425
Shikhar15606 <shikharshukla15606@gmail.com>
2021-06-06 14:04:00 +0530

    storing products on realtime db

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,10 +7,27 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  // modern js syntax if the key and value name is same
-  return {
-    type: CREATE_PRODUCT,
-    productData: { title, description, imageUrl, price },
+  return async dispatch => {
+    // execute any async code here and then dispatch action
+    const response = await fetch(
+      'https://shopify15606-default-rtdb.firebaseio.com/products.json',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price,
+        }),
+      }
+    );
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: { id: resData.name, title, description, imageUrl, price },
+    });
   };
 };
 
